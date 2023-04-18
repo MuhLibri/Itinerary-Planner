eb54ad6393663146bdea572d5deac0290679397f
irgimondo <89301345+irgimondo@users.noreply.github.com>
2023-04-19 06:20:13 +0700

    Update ui_Edititinerary.py

--- src/ui_Edititinerary.py
+++ src/ui_Edititinerary.py
@@ -1,71 +1,84 @@
-# -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QGridLayout, QLineEdit, QPushButton, QInputDialog, QTextBrowser
-from PyQt5.QtCore import pyqtSlot
-import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MyForm(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('Edit Itinerary Planner')
-        self.setGeometry(100, 100, 503, 661)
-
-        widget = QWidget()
-        self.setCentralWidget(widget)
-
-        gridLayout = QGridLayout()
-        widget.setLayout(gridLayout)
-
-        self.label = QtWidgets.QLabel(self)
-        self.label.setGeometry(QtCore.QRect(0, 0, 501, 141))
-        self.label.setStyleSheet("font: 18pt \"MS Shell Dlg 2\";color: rgb(255, 255, 255);\n"
-"background-color: rgb(85, 170, 255);")
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(480, 640)
+        self.widget = QtWidgets.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(0, 0, 471, 631))
+        self.widget.setStyleSheet("background-color:rgb(255, 255, 255)")
+        self.widget.setObjectName("widget")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setGeometry(QtCore.QRect(6, 10, 461, 121))
+        self.label.setStyleSheet("\n"
+"font: 15pt \"MS Shell Dlg 2\";\n"
+"background-color: rgb(85, 170, 255);\n"
+"color: rgb(255, 255, 255);")
         self.label.setObjectName("label")
-        self.label.setText("       Edit Itinerary Planner")
+        self.textEdit = QtWidgets.QTextEdit(self.widget)
+        self.textEdit.setGeometry(QtCore.QRect(10, 140, 461, 101))
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit_2 = QtWidgets.QTextEdit(self.widget)
+        self.textEdit_2.setGeometry(QtCore.QRect(10, 250, 461, 101))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.textEdit_3 = QtWidgets.QTextEdit(self.widget)
+        self.textEdit_3.setGeometry(QtCore.QRect(10, 360, 461, 101))
+        self.textEdit_3.setObjectName("textEdit_3")
+        self.pushButton = QtWidgets.QPushButton(self.widget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 20, 51, 31))
+        self.pushButton.setStyleSheet("color: rgb(255, 255, 255);\n"
+"background-color: rgb(85, 170, 255);\n"
+"font: 16pt \"MS Shell Dlg 2\";")
+        self.pushButton.setObjectName("pushButton")
 
-        self.textBrowser = QTextBrowser(self)
-        self.textBrowser.setGeometry(QtCore.QRect(10, 150, 256, 121))
-        self.textBrowser.setStyleSheet("border-right-color: rgb(0, 0, 0);\n"
-"border-color: rgb(85, 0, 255);\n"
-"border-top-color: rgb(0, 85, 255);")
-        self.textBrowser.setObjectName("textBrowser")
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
 
-        self.textBrowser_2 = QTextBrowser(self)
-        self.textBrowser_2.setGeometry(QtCore.QRect(10, 320, 256, 121))
-        self.textBrowser_2.setStyleSheet("border-color: rgb(0, 0, 0);\n"
-"gridline-color: rgb(0, 0, 0);")
-        self.textBrowser_2.setObjectName("textBrowser_2")
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "Edit Itinerary Planner"))
+        self.textEdit.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.875pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">Gedung Sate</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:9pt;\">Kota Bandung, Indonesia</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Mar 28, 2023</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Mobil</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Ubah</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Hapus</p></body></html>"))
+        self.textEdit_2.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.875pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">Taman Hutan Raya</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:9pt;\">Kota Bandung, Indonesia</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Mar 29, 2023</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Motor</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Ubah</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Hapus</p></body></html>"))
+        self.textEdit_3.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.875pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt; font-weight:600;\">Bandung Electronic Center</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:9pt;\">Kota Bandung, Indonesia</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Mar 29, 2023</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Motor</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Ubah</p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Hapus</p></body></html>"))
+        self.pushButton.setText(_translate("Form", "X"))
 
-        self.pushButton1 = QPushButton('Ubah', self)
-        self.pushButton2 = QPushButton('Hapus', self)
-        self.pushButton1.setGeometry(QtCore.QRect(20, 280, 75, 23))
-        self.pushButton2.setGeometry(QtCore.QRect(140, 280, 75, 23))
-        self.pushButton1.setStyleSheet("background-color: rgb(85, 170, 255);")
-        self.pushButton2.setStyleSheet("background-color: rgb(85, 170, 255);")
-        self.pushButton1.setObjectName("pushButton1")
-        self.pushButton2.setObjectName("pushButton2")
-        self.pushButton1.clicked.connect(self.ubahTeks)
-        self.pushButton2.clicked.connect(self.hapusTeks)
 
-        self.show()
-
-    @pyqtSlot()
-    def ubahTeks(self):
-        teksBaru, ok = QInputDialog.getText(self, 'Ubah Teks', 'Masukkan teks baru:')
-        if ok:
-            self.textBrowser.setText(teksBaru)
-
-    @pyqtSlot()
-    def hapusTeks(self):
-        self.textBrowser.setText('')
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    form = MyForm()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
